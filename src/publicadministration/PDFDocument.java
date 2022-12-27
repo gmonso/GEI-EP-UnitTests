4558ab769f6d0f2a292433d7f60c0507760cd08e
Gerard <gerardmonso@gmail.com>
2022-12-27 18:23:22 +0100

    new functions pdfdocument class

--- src/publicadministration/PDFDocument.java
+++ src/publicadministration/PDFDocument.java
@@ -2,13 +2,18 @@ package publicadministration;
 
 import data.DocPath;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Date;
 
 public class PDFDocument {
     private final Date creatDate;
-    private final DocPath path;
+    private DocPath path;
     private final File file;
     // Initializes attributes and emulates the document download at a default path
 
@@ -43,9 +48,27 @@ public class PDFDocument {
     // To implement only optionally
     // Moves the document to the destination path indicated
     public void moveDoc(DocPath destPath) throws IOException {
+        if (destPath == null) {
+            throw new IOException("The destination path is null");
+        }
+        try {
+
+            Files.move((Path) this.path, (Path) destPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            // handle the exception
+        }
+        this.path = destPath;
+
     }
 
     // Opens the document at the path indicated
     public void openDoc(DocPath path) throws IOException {
+        if (path == null) throw new IOException("The path is null");
+        try {
+            File file = new File (path.toString());
+            Desktop.getDesktop().open(file);
+        } catch (IOException ex) {
+            throw new IOException("The file cannot be opened");
+        }
     }
 }
