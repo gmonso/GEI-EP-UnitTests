5c976594232174e859e1d8778bbf41ceddda3c55
Faysal Badaoui <faysalbadaoui02@gmail.com>
2022-12-30 15:12:49 +0100

    pase testos un altre cop, fallaven coses

--- src/publicadministration/CrimConvictionsColl.java
+++ src/publicadministration/CrimConvictionsColl.java
@@ -5,20 +5,17 @@ import exceptions.NullArgumentException;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
-
-public class CrimConvictionsColl {
-    // Represents the total criminal convictions registered for a citizen
+import publicadministration.*;
+public class CrimConvictionsColl { // Represents the total criminal convictions registered for a citizen
+    // Its components, that is, the set of criminal convictions
     private final Set<CrimConviction> crimConvs;
 
 
     public CrimConvictionsColl() {
         this.crimConvs = new HashSet<>();
-        this.numConvs = 0;
-    }
+    } // Initializes the object
 
-    public int getNumConvs() {
-        return this.numConvs;
-    }
+    // the getters --V
 
     public Set<CrimConviction> getCrimConvs() {
         return this.crimConvs;
@@ -30,23 +27,18 @@ public class CrimConvictionsColl {
         if(crmC == null) throw new NullArgumentException("Crim Conv Coll value Is null");
         if(correctFOrm(crmC))
             this.crimConvs.add(crmC);
-        }
-        
-   private boolean correctFOrm(CrimConviction crmC) throws NullArgumentException {
+    }
+
+    private boolean correctFOrm(CrimConviction crmC) throws NullArgumentException {
         if(crmC.getDate() == null || crmC.getOffense() == null || crmC.getSentence() == null){
             throw new NullArgumentException("Element a CrimConv null");
         }
         return true;
     }
 
-
-    public void addCriminalConviction(CrimConviction crmC) {
-        this.crimConvs.add(crmC);
-        this.numConvs++;
-    }
-
-    public CrimConviction getCriminalConviction(Date date) {
-
+    // Adds a criminal conviction
+    public CrimConviction getCriminalConviction(Date date) throws NullArgumentException {
+        if(date == null) throw new NullArgumentException("Date value Is null");
         for (CrimConviction crim : this.crimConvs) {
             if (crim.getDate().equals(date)) {
                 return crim;
@@ -54,6 +46,8 @@ public class CrimConvictionsColl {
         }
         return null;
     }
+
+    // Gets a specific criminal conviction by date
 
     @Override
     public String toString() {
