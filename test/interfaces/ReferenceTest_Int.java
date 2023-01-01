b01e55a8bdd3629db93293eebde179ca6118a217
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-01-01 18:46:31 +0100

    All data tests revised

--- test/interfaces/ReferenceTest_Int.java
+++ test/interfaces/ReferenceTest_Int.java
@@ -2,10 +2,14 @@ package interfaces;
 
 import exceptions.NullArgumentException;
 import exceptions.WrongFormatException;
+import org.junit.jupiter.api.Test;
 
 public interface ReferenceTest_Int {
 
     void correctReference() throws NullArgumentException, WrongFormatException;
+
+    @Test
+    void setterReference() throws NullArgumentException, WrongFormatException;
 
     void nullReference();
 
